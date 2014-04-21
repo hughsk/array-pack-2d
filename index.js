e4314dcf133e8b0bff6bc59e2004ee5a9825c51d
Hugh Kennedy <hughskennedy@gmail.com>
2014-04-21 16:40:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var dtype = require('dtype')
+
+module.exports = pack
+
+function pack(arr, type) {
+  type = type || 'float32'
+
+  var Arr = typeof type === 'string'
+    ? dtype(type)
+    : type
+
+  var dim = arr[0].length
+  var out = new Arr(arr.length * dim)
+  var k = 0
+
+  for (var i = 0; i < arr.length; i++)
+  for (var j = 0; j < dim; j++) {
+    out[k++] = arr[i][j]
+  }
+
+  return out
+}
